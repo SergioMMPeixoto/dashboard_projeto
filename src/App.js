3045148070e2bf781a4eca9ffae4a217b11bdcca
SergioMMPeixoto <peixotosergio0@gmail.com>
2022-07-29 16:37:26 +0100

    Testes no App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,61 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import './App.css'
 
 const App = () => {
+  const activeMenu = false;
+
   return (
-    <h1 className='underline text-3x1'>App</h1>
+    <div>
+      <BrowserRouter>
+      <div className='flex relative dark:bg-main-dark-bg'>
+        <div className='fixed right-4 bottom-4' style={{zIndex: '1000'}}>
+          <TooltipComponent content="Definições" position='Top'>
+            <button type="button" className='text-3x1 p-3 hover:drop-shadow-x1 hover:bg-light-gray text-white'
+            style={{background: 'blue', borderRadius: '50%'}}>
+              <FiSettings />
+            </button>
+          </TooltipComponent>
+        </div>
+        {activeMenu ? (
+          <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+            Sidebar
+          </div>
+        ) : (
+          <div className='w-0 dark:bg-secondary-dark-bg'>
+            Invisivel
+          </div>
+        )}
+        <div className={
+          `dark:bg-main-bg bg-main-bg main-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
+          }>
+          
+          <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+            Navbar
+          </div>
+        </div>
+        <Routes>
+          {/*  Dashboard principal  */}
+          <Route path="/" element="Home" />
+          <Route path="/home" element="Home" />
+
+          {/* Paginas */}
+          <Route path="/inventario" element="Inventario" />
+          <Route path="/importar" element="Importar" />
+
+          {/* Apps */}
+          <Route path="/calendario" element="Calendário" />
+
+          {/* Grafos */}
+          <Route path="/desperdicio" element="Desperdicio" />
+          <Route path="/consumo" element="Consumo" />
+        </Routes>
+      </div>
+      </BrowserRouter>
+    </div>
   )
 }
 
